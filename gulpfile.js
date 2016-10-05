e94e8bd1723a1b2ac701c72373c38892327b2b5e
Maira Bello <maira.araujo@liferay.com>
2016-10-05 16:01:19 -0300

    Includes build files on published npm package

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,16 @@
 'use strict';
 
+var gulp = require('gulp');
 var metal = require('gulp-metal');
+var runSequence = require('run-sequence');
 
 metal.registerTasks({
 	bundleCssFileName: 'router.css',
 	bundleFileName: 'router.js',
 	mainBuildJsTasks: ['build:globals'],
 	moduleName: 'metal-router'
+});
+
+gulp.task('default', function(done) {
+	runSequence('clean', ['build:globals', 'build:amd'], 'uglify', done);
 });
