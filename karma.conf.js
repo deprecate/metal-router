f1d7e125c38a20460b50a25c7cdcc0b141ca2aca
Chema Balsas <jose.balsas@liferay.com>
2017-09-04 18:47:40 +0200

    Adds karma matchers for metal package

--- karma.conf.js
+++ karma.conf.js
@@ -4,6 +4,8 @@ var metalKarmaConfig = require('metal-karma-config');
 
 module.exports = function (config) {
 	metalKarmaConfig(config);
+	config.files.push('node_modules/metal/src/**/*.js');
 	config.files.push('node_modules/senna/src/**/*.js');
+	config.preprocessors['node_modules/metal/src/**/*.js'] = ['babel', 'commonjs'];
 	config.preprocessors['node_modules/senna/**/*.js'] = ['babel', 'commonjs'];
 };
